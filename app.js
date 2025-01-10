c4cf1ad7e60b25a163ade13071c3d731f15802c8
mrhaddan <nick.haddan@gmail.com>
2025-01-09 16:48:13 -0800

    Added nodemailer for contact form

--- app.js
+++ app.js
@@ -3,6 +3,40 @@ const app = express();
 const port = 3000;
 const path = require('path');
 const ejsMate = require('ejs-mate');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    host: 'smtp.gmail.com',
+    port: 465  ,
+    secure: true,
+    auth: {
+        user: 'nickwiseowl@gmail.com',
+        pass: process.env.GmailPass,
+    }
+});
+
+async function main() {
+    // send mail with defined transport object
+    const mailOptions = await transporter.sendMail({
+        from: ' "Wise Owl Plans" <nick@wiseowlplans.com>', // sender address
+        to: "nick@wiseowlplans.com", // list of receivers
+        subject: "Hello",
+        text: "This is a test?",
+        html: "<b>Hello World?</b>"
+    });
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if(error) {
+            console.error("error sending email: ", error);
+        } else{ 
+            console.log("Email sent: ", info.response);
+        }
+    });
+}
 
 
 // Sets an almost global path for css and js files
