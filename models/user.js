220b72a2d3e05c0f856771620f21f52f5c8dbd9e
mrhaddan <nick.haddan@gmail.com>
2025-02-27 05:32:04 -0800

    Added admin property to user model

--- models/user.js
+++ models/user.js
@@ -7,6 +7,10 @@ const UserSchema = new Schema({
         type: String,
         required: true,
         unique: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
     }
 })
 
